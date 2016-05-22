e8e31c7599f2e98bc046bb19a8a9dece16ba45c8
XueShan <289159468@qq.com>
2016-05-22 10:41:18 +0800

    1.0.2

--- index.js
+++ index.js
@@ -1,55 +1,7 @@
-//vi maft/index.js
-var fis = module.exports =  require('fis3');
+/*
+ * fis 模块入口
+ */
 
-fis.require.prefixes.unshift('maft');
-fis.cli.name = 'maft';
-fis.cli.info = require('./package.json');
+'use strict';
 
-fis.match('*', {
-  release: '/static/$0' // 所有资源发布时产出到 /static 目录下
-});
-
-fis.match('*.php', {
-  release: '/template/$0' // 所有 PHP 模板产出后放到 /template 目录下
-});
-
-// 所有js, css 加 hash
-fis.match('*.{js,css,less}', {
-  useHash: true
-});
-
-// 所有图片加 hash
-fis.match('image', {
-  useHash: true
-});
-
-// fis-parser-less
-fis.match('*.less', {
-  parser: fis.plugin('less'),
-  rExt: '.css'
-});
-
-fis.match('*.js', {
-  optimizer: fis.plugin('uglify-js')
-});
-
-fis.match('*.{css,less}', {
-  optimizer: fis.plugin('clean-css')
-});
-
-fis.match('*.png', {
-  optimizer: fis.plugin('png-compressor')
-});
-
-fis.match('widget/*.{php,js,css}', {
-  isMod: true
-});
-
-fis.match('::package', {
-  spriter: fis.plugin('csssprites')
-});
-
-//fis3-hook-module
-fis.hook('module', {
-  mode: 'amd' // 模块化支持 amd 规范，适应 require.js
-});
+module.exports = require('./lib/fis.js');
